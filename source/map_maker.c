b6af8f9a56ca8ddec9a63a54eed9f70d1cb91a86
CasparEkroth <caspar.ekroth@gmail.com>
2025-02-17 08:59:12 +0100

    adding to map_maker

--- source/map_maker.c
+++ source/map_maker.c
@@ -1,1 +1,27 @@
 #include "map_maker.h"
+//input 
+//render 
+//seving to file 
+void maker(MapMaker *pMapMaker, Game *pGame){
+    while (pMapMaker->isMakingMap){
+        if(!pMapMaker->isMakingMap) return;
+        maker_input(pGame->pControls);
+        maker_update(pMapMaker);
+        maker_render(pGame->pRenderer,pMapMaker,pGame->pMap);
+    }
+    
+
+}
+
+void maker_render(SDL_Renderer *pRendererer,MapMaker *pMapMaker,Map *pMap){
+
+}
+
+void maker_update(MapMaker *pMapMaker){
+
+}
+
+
+void maker_input(ScreenAndInput *pControls){
+
+}
--- source/map_maker.h
+++ source/map_maker.h
@@ -3,4 +3,22 @@
 #include "game.h"
 
 
+typedef struct {
+    char selectedTile;
+    char fileName[NAME];
+    char map[NUMMBER_OF_TILSE_Y][NUMMBER_OF_TILSE_X];
+    SDL_Rect rect_map[NUMMBER_OF_TILSE_Y][NUMMBER_OF_TILSE_X];
+    bool isSavede,isChosingNewTile,isMakingMap;
+}MapMaker;
+
+
+void maker(MapMaker *pMapMaker, Game *pGame);
+void maker_input(ScreenAndInput *pControls);
+void maker_update(MapMaker *pMapMaker);
+void maker_render(SDL_Renderer *pRendererer,MapMaker *pMapMaker,Map *pMap){
+
+
 #endif
+
+// det kommer att var en selekt funktion där man sedan plaserar ut de 
+// tiles man vill och därefter väljer en till 
